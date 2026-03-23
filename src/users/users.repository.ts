441f4e15191dd2bba67a02a722a12919f7a5cbe5
Rashad Dweik <rashaddweik20@gmail.com>
2026-03-23 15:21:32 +0200

    removed password has field from db

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient , Prisma , User } from '../../generated/prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 import UserSummaryDto from './dto/user-summary-dto';
 
 @Injectable()
@@ -9,6 +9,19 @@ export class UsersRepository {
     async userById(id: string) : Promise<UserSummaryDto | null> {
         const user = await this.prisma.user.findUnique({
             where: { id },
+            select: {
+                id: true,
+                fullName: true,
+                email: true,
+                role: true
+            }
+        });
+        return user;
+    }
+
+    async userByEmail(email: string) : Promise<UserSummaryDto | null> {
+        const user = await this.prisma.user.findUnique({
+            where: { email },
             select: {
                 id: true,
                 fullName: true,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository) {}
+
+    async getUserById(id: string) {
+        return this.usersRepository.userById(id);
+    }
+
+    async getUserByEmail(email: string) {
+        return this.usersRepository.userByEmail(email);
+    }
+
+}
